91b6f29970172de31f04c15164972dc493097e87
amigos-cosmin <cosmin@techamigos.co>
2023-02-06 15:38:28 +0200

    test resource

--- src/index.tsx
+++ src/index.tsx
@@ -1,60 +1,59 @@
 import * as React from "react";
 import { ApplicationSet } from "./model/applicationset";
-import { Tree, Application, Node } from "./model/tree";
-import axios from "axios";
+import { Tree, Application } from "./model/tree";
 
-interface ApiManifestCallParams {
-  name: string;
-  appNamespace: string;
-  namespace: string;
-  resourceName: string;
-  version: string;
-  kind: string;
-  group: string;
-}
+// interface ApiManifestCallParams {
+//   name: string;
+//   appNamespace: string;
+//   namespace: string;
+//   resourceName: string;
+//   version: string;
+//   kind: string;
+//   group: string;
+// }
 
 export const Extension = (props: {
   tree: Tree;
-  app: ApplicationSet;
+  resource: ApplicationSet;
   application: Application;
 }) => {
-  let report = {} as Node;
+  //let report = {} as Node;
 
-  console.log(props);
-  props.tree.nodes.forEach((element) => {
-    if (element.name == props.app.metadata.name) {
-      report = element;
-    }
-  });
-  console.log(report);
-  let call: ApiManifestCallParams = {
-    appNamespace: props.application.metadata.namespace,
-    group: report.group,
-    kind: report.kind,
-    name: report.name,
-    namespace: report.namespace,
-    resourceName: report.name,
-    version: report.version,
-  };
+  console.log(props.resource);
+  // props.tree.nodes.forEach((element) => {
+  //   if (element.name == props.app.metadata.name) {
+  //     report = element;
+  //   }
+  // });
+  // console.log(report);
+  // let call: ApiManifestCallParams = {
+  //   appNamespace: props.application.metadata.namespace,
+  //   group: report.group,
+  //   kind: report.kind,
+  //   name: report.name,
+  //   namespace: report.namespace,
+  //   resourceName: report.name,
+  //   version: report.version,
+  // };
 
-  const url =
-    "https://127.0.0.1:59935/api/v1/applications/" +
-    props.application.metadata.name +
-    "/resource";
+  // const url =
+  //   "https://127.0.0.1:59935/api/v1/applications/" +
+  //   props.application.metadata.name +
+  //   "/resource";
 
-  const res = ApiCall(call, url);
-  let data = {} as any;
-  res
-    .then((value) => {
-      data = value;
-      console.log("AAA", value);
-      // Expected output: "Success!"
-    })
-    .catch((err) => {
-      console.log("err", err);
-    });
+  // const res = ApiCall(call, url);
+  // let data = {} as any;
+  // res
+  //   .then((value) => {
+  //     data = value;
+  //     console.log("AAA", value);
+  //     // Expected output: "Success!"
+  //   })
+  //   .catch((err) => {
+  //     console.log("err", err);
+  //   });
 
-  console.log(data);
+  // console.log(data);
   return (
     <div>
       <p style={{ color: "red" }}>DATA MAN</p>
@@ -63,17 +62,17 @@ export const Extension = (props: {
 };
 
 export const component = Extension;
-async function ApiCall(params: ApiManifestCallParams, url: string) {
-  const res = await axios.get(url, {
-    params: {
-      name: params.name,
-      appNamespace: params.appNamespace,
-      namespace: params.namespace,
-      resourceName: params.resourceName,
-      version: params.version,
-      kind: params.kind,
-      group: params.group,
-    },
-  });
-  return await JSON.parse(res.data.manifest);
-}
+// async function ApiCall(params: ApiManifestCallParams, url: string) {
+//   const res = await axios.get(url, {
+//     params: {
+//       name: params.name,
+//       appNamespace: params.appNamespace,
+//       namespace: params.namespace,
+//       resourceName: params.resourceName,
+//       version: params.version,
+//       kind: params.kind,
+//       group: params.group,
+//     },
+//   });
+//   return await JSON.parse(res.data.manifest);
+// }
