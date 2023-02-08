c1c662361006749fd14537f5e8da99ba82c5733b
amigos-cosmin <cosmin@techamigos.co>
2023-02-08 16:15:44 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { ConfigAuditReport, VulnerabilityReport } from "./model/Types";
 import { Tree, Application } from "./model/tree";
-//import ConfigAuditReportComponent from "./components/ConfigAuditReportComponent";
+import ConfigAuditReportComponent from "./components/ConfigAuditReportComponent";
 import "./styles.css";
 
 export const Extension = (props: {
@@ -9,16 +9,32 @@ export const Extension = (props: {
   resource: ConfigAuditReport | VulnerabilityReport;
   application: Application;
 }) => {
-  console.log(props.resource, typeof props.resource);
+  console.log(props.resource);
 
   return (
     <div>
       <p className="font-semibold text-black text-xl text-center p-8">
         {props.resource.kind}
       </p>
-      {/* <ConfigAuditReportComponent reportData={data} data={props.application} /> */}
+
+      {ReportType(props.resource, props.application)}
     </div>
   );
 };
 
 export const component = Extension;
+
+function ReportType(
+  report: ConfigAuditReport | VulnerabilityReport,
+  app: Application
+) {
+  let data;
+  switch (report.kind) {
+    case "ConfigAuditReport":
+      data = report as ConfigAuditReport;
+      return <ConfigAuditReportComponent reportData={data} data={app} />;
+    case "VulnerabilityReport":
+      data = report as VulnerabilityReport;
+      return <div>{data.report.registry.server}</div>;
+  }
+}
